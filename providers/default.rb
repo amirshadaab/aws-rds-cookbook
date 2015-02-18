1514daca1aa42a4015be542c61311442385d7917
amirshadaab <mohdamirshadaab@gmail.com>
2015-02-18 13:22:39 -0600

    Update default.rb
    Adding action for RDS creation from Snapshot in provider

--- providers/default.rb
+++ providers/default.rb
@@ -20,6 +20,20 @@ action :create do
   end
 end
 
+action :createFromSnapshot do
+  if @current_resource.exists
+    Chef::Log.info "#{@new_resource} already exists - nothing to do."
+    set_node_attrs
+  else
+    converge_by "Create #{@new_resource}" do
+      Chef::Log.info "Creating #{new_resource}. This could take upto 10 minutes"
+      create_instance_from_snapshot(@new_resource.id)
+      Chef::Log.info "Created #{@new_resource}"
+      set_node_attrs
+    end
+  end
+end
+
 
 def load_current_resource
   @current_resource = Chef::Resource::AwsRds.new(new_resource.id)  
